028ca68c7d70e37825ea87b7d89053801767f4c5
Sunday Oladejo <sundayoladejo13@gmail.com>
2023-02-16 12:38:43 +0100

    finished discount grpc development

---
+++ src/Services/Discount/Discount.API/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Discount.API.Repositories;
+
+namespace Discount.API
+{
+    public class Startup
+    {
+        public static IConfiguration StaticConfig { get; private set; }
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            StaticConfig = configuration;
+            Environment = environment;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            //Configure Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+            });
+
+            // register services
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            //app.MapControllers();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
+using Microsoft.AspNetCore.Builder;
+
+namespace Discount.Grpc
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // register services
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
+            services.AddAutoMapper(typeof(Startup));
+            services.AddGrpc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<DiscountService>();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC must be made though a");
+                });
+            });
+        }
+    }
+}
